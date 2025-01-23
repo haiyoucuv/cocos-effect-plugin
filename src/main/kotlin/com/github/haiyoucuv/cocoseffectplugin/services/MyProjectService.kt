26a1102f5b76daeb57eb99fcaeb18294ebae60f6
GitHub Action <action@github.com>
2025-01-23 08:43:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/haiyoucuv/cocoseffectplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/haiyoucuv/cocoseffectplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.haiyoucuv.cocoseffectplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.haiyoucuv.cocoseffectplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
